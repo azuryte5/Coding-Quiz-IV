ae59675e86b0908dee9a57631829b033e86295c9
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-13 16:26:56 -0400

    Setting this up

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// GIVEN I am taking a code quiz
+
+// WHEN I click the start button
+//button will be qued to start 
+// [ ]THEN a timer starts and I am presented with a question
+// intervals and ticking down
+// WHEN I answer a question
+// hover elements, array and click function 
+// [ ]THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+
+// [ ]THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+
+// [ ]THEN the game is over
+
+// WHEN the game is over
+
+// [ ]THEN I can save my initials and score
