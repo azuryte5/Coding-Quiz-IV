301aabd2aa1eab1babc689e196bd63c4534767fa
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-14 13:30:07 -0400

    Timer working by button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,42 @@
 // GIVEN I am taking a code quiz
+var timer = 108;
+var gameStartBtn = document.getElementById('start');
 
+
+
+
+var countdown = function() {
+    var scoreClockEl = document.getElementById("scoreClock")
+    timer--;
+    scoreClockEl.textContent=timer
+    console.log(timer)
+//condition where timer is lost by 10
+    if (timer == 90){
+        timer= timer -10
+    }
+    if (timer ===0){
+        console.log("Game Over")
+        clearInterval(startGame)
+    }
+}
+
+var startGame=function(){
+    setInterval(countdown, 1000)
+};
+console.log(gameStartBtn)
+gameStartBtn.onclick = startGame;
+
+
+//var counter = 10
+// var countdown = function (){
+//     counter--;
+//       console.log(counter);
+//   if (counter === 0) {
+//         console.log("blastoff");
+//         clearInterval(startCountdown);
+//     };
+// };
+// var startCountdown = setInterval(countdown, 1000);
 // WHEN I click the start button
 //button will be qued to start 
 // [ ]THEN a timer starts and I am presented with a question
