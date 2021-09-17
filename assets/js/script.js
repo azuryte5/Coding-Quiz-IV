381aca1e99ccee61579f05c2aba5c9c1b0e1be40
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-17 13:12:15 -0400

    button advances count!

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,19 @@ var gameNewBtnEl = document.getElementById('eventWindow');
 gameNewBtnEl.innerHTML="<button id='start'>Start Button</button>";
 var optionSpot=document.getElementById("options")
 var buttonLocation = document.querySelector("#eventWindow");
-var count = 0
+var count = -1
 
 
 var question = function () {
 gameNewBtnEl.innerHTML="";
+count++
 
 questionArray=[
     {q:"what is an apple?", a:"fruit", b:"b", c:"c", d:"d"},
-    {q:"What is a carrot?", a:"a",b:"b", c:"vegetable", d:"d"},{},
-    {q:"What is a steak?", a:"meat",b:"b", c:"c", d:"d"},{},{},{},
-    {q:"What is IceCream?", a:"a",b:"b", c:"c", d:"treat"},{},{},{},{},{},{},
-    {q:"What is bread?", a:"a",b:"grain", c:"c", d:"d"}];
+    {q:"What is a carrot?", a:"a",b:"b", c:"vegetable", d:"d"},
+    {q:"What is a steak?", a:"meat",b:"b", c:"c", d:"d"},
+    {q:"What is IceCream?", a:"a",b:"b", c:"c", d:"treat"},
+    {q:"What is bread?", a:"a",b:"grain", c:"c", d:"d"}]
 
 
 var questionDisplay = document.getElementById("question");
@@ -30,63 +31,35 @@ optionListBEl.innerHTML="<button>"+ questionArray[count].b +"</button>";
 optionListCEl.innerHTML="<button>"+ questionArray[count].c +"</button>";
 optionListDEl.innerHTML="<button>"+ questionArray[count].d +"</button>";
 
-optionListAEl.addEventListener("click", function () {
-console.log(count) 
-if (count === 0||count === 3){
-    count++;
-    question();
-} else {wrongAnswer()}
-});
 
-optionListBEl.addEventListener("click", function () {
-console.log(count) 
-if (count === 14){
-    count++;
-    alert("Gameover");
-} else {wrongAnswer()}
-});
-optionListCEl.addEventListener("click", function () {
-console.log(count) 
-if (count === 1){
-    count++;
-    question();
-} else {wrongAnswer()}
-});  
-    
-optionListDEl.addEventListener("click", function () {
-console.log(count) 
-if (count === 7){
-    count++;
-    question();
-} else {wrongAnswer()}
-});
-} 
-
-
-var countdown = function() {
-startButton = document.querySelector("#start")
-
-    var scoreClockEl = document.getElementById("scoreClock")
-    timer--;
-    scoreClockEl.textContent=timer
-   
+optionListAEl.onclick = function (){
+    if (count === 1|| count === 3 || count === 4){
+    wrongAnswer()}    
+    console.log(count); question()
 }
+optionListBEl.onclick = function (){
+    if (count === 0|| count === 1 || count === 2|| count === 3){
+        wrongAnswer()}    
+    console.log(count); question()
+}
+optionListCEl.onclick = function (){
+    if (count === 0|| count === 2 || count === 3|| count === 4){
+        wrongAnswer()}    
+    console.log(count); question()
+}
+optionListDEl.onclick = function (){
+    if (count === 0|| count === 1 || count === 2 || count == 3){
+        wrongAnswer()}    
+    console.log(count); question()
+}}
 
-wrongAnswer = function() { 
-// wrongAnswerDisplay = 5     
-timer= timer -10
-
+wrongAnswer = function() {    
+timer = timer -10
 var answerDisplay = document.getElementById('eventWindow');
 answerDisplay.textContent="Wrong Answer";
+console.log(answerDisplay);
 answerDisplay.setAttribute("style", "color:var(--secondary-colour");
 //wrongAnswerDisplay--;
-count++;
-question();
-}
-
-if (timer ==0){
-    console.log("Game Over")
-    clearInterval(countdown)
 }
 
 var startGame=function(){
@@ -107,38 +80,23 @@ optionSpot.appendChild(optionListBEl);
 optionSpot.appendChild(optionListCEl);
 optionSpot.appendChild(optionListDEl);
 question()
+
+if (timer >= 0) {
+    clearInterval(countdown)
+}
 };
 
 start.onclick = startGame;
 
 
+function countdown(){
+   if (timer >1) {
+      scoreClock.textContent ='You have ' + timer + ' second(s) left';
+      timer--;}
+    else {
+      scoreClock.textContent = 'Game over score at ' + timer;
+      questionDisplay.innerHTML="";
+      //scorePage();
+    }
+  };
 
-//var counter = 10
-// var countdown = function (){
-//     counter--;
-//       console.log(counter);
-//   if (counter === 0) {
-//         console.log("blastoff");
-//         clearInterval(startCountdown);
-//     };
-// };
-// var startCountdown = setInterval(countdown, 1000);
-// WHEN I click the start button
-//button will be qued to start 
-// [ ]THEN a timer starts and I am presented with a question
-// intervals and ticking down
-// WHEN I answer a question
-// hover elements, array and click function 
-// [ ]THEN I am presented with another question
-
-// WHEN I answer a question incorrectly
-
-// [ ]THEN time is subtracted from the clock
-
-// WHEN all questions are answered or the timer reaches 0
-
-// [ ]THEN the game is over
-
-// WHEN the game is over
-
-// [ ]THEN I can save my initials and score
